3b882303de777a6d0738535ad31ded75fdf669d4
Ciaran Shiels <cas.ireland@gmail.com>
2024-02-11 10:59:36 +0000

    Adding FIle

---
+++ main.py
@@ -0,0 +1,18 @@
+# main.py
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Simple API"}
+
+
+@app.get("/ep-1")
+async def root():
+    return {"message": "Simple API-1"}
+
+@app.get("/ep-2")
+async def root():
+    return {"message": "Simple API-2"}
